f9f1e093a656a7df2b4b466c87ac12b0ab702066
bitchenhui <13261281092@163.com>
2023-11-10 16:06:04 +0800

    fix：fix x86 xcode build

--- build/xcode/make-project.sh
+++ build/xcode/make-project.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-cmake -G "Xcode" ../../source && ccmake ../../source
+###
+ # @Author: zhangda
+ # @Date: 2023-10-21 16:53:21
+ # @LastEditors: bitchenhui 13261281092@163.com
+ # @LastEditTime: 2023-11-10 16:02:00
+ # @FilePath: /x265_codec/build/xcode/make-project.sh
+ # @Description: 
+ # 
+ # Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+### 
+cmake  -T "buildsystem=1" -H. -GXcode ../../source && ccmake ../../source 
